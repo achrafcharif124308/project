22a5aafb1caf77303c404cae606653ccb28850e3
achrafcharif124308 <84228235+achrafcharif124308@users.noreply.github.com>
2023-06-08 13:30:56 +0300

    Add files via upload

---
+++ projetjs.js
@@ -0,0 +1,455 @@
+
+document.getElementById("iphone1").addEventListener("mouseover",function(e){
+	
+	e.target.src="apple-iphone-13-pro-max-2.jpg"
+});
+document.getElementById("iphone1").addEventListener("mouseout",function(e){
+
+	e.target.src="iphone-13-pro-max.jpg"
+});
+
+
+document.getElementById("iphone2").addEventListener("mouseover",function(e){
+	
+	e.target.src="iphone-13.jpg"
+});
+document.getElementById("iphone2").addEventListener("mouseout",function(e){
+	
+	e.target.src="iphone13-2.jpg"
+});
+
+
+document.getElementById("iphone3").addEventListener("mouseover",function(e){
+	
+	e.target.src="iphone-11-pro-max_2.jpg"
+});
+document.getElementById("iphone3").addEventListener("mouseout",function(e){
+	
+	e.target.src="iphone-11-pro-max.jpg"
+});
+
+
+document.getElementById("iphone4").addEventListener("mouseover",function(e){
+	
+	e.target.src="iphone11-2.jpg"
+});
+document.getElementById("iphone4").addEventListener("mouseout",function(e){
+	
+	e.target.src="iphone-11.png"
+});
+
+
+
+document.getElementById("samsung1").addEventListener("mouseover",function(e){
+	
+	e.target.src="galaxy-s21-ultra-5g-2.jpg"
+});
+document.getElementById("samsung1").addEventListener("mouseout",function(e){
+
+	e.target.src="galaxy-s21-ultra-5g.png"
+});
+
+
+document.getElementById("samsung2").addEventListener("mouseover",function(e){
+	
+	e.target.src="Samsung-Galaxy-S21-FE-5G-2.jpg"
+});
+document.getElementById("samsung2").addEventListener("mouseout",function(e){
+	
+	e.target.src="Samsung-Galaxy-S21-FE-5G.jpg"
+});
+
+
+document.getElementById("samsung3").addEventListener("mouseover",function(e){
+	
+	e.target.src="galaxy-z-fold3-2.png"
+});
+document.getElementById("samsung3").addEventListener("mouseout",function(e){
+	
+	e.target.src="Galaxy-Z-Fold3.jpg"
+});
+
+
+document.getElementById("samsung4").addEventListener("mouseover",function(e){
+	
+	e.target.src="Samsung-Galaxy-A52-2.jpg"
+});
+document.getElementById("samsung4").addEventListener("mouseout",function(e){
+	
+	e.target.src="Galaxy-A52.jpg"
+});
+
+
+document.getElementById("huawei1").addEventListener("mouseover",function(e){
+	
+	e.target.src="huawei-p40-pro-2.png"
+});
+document.getElementById("huawei1").addEventListener("mouseout",function(e){
+
+	e.target.src="p40-pro.png"
+});
+
+
+document.getElementById("huawei2").addEventListener("mouseover",function(e){
+	
+	e.target.src="Huawei-Mate-40-pro.jpg"
+});
+document.getElementById("huawei2").addEventListener("mouseout",function(e){
+	
+	e.target.src="Huawei-Mate-40-pro-2.jpg"
+});
+
+
+document.getElementById("huawei3").addEventListener("mouseover",function(e){
+	
+	e.target.src="huawei-mate-x2-2.png"
+});
+document.getElementById("huawei3").addEventListener("mouseout",function(e){
+	
+	e.target.src="Huawei_mate_x2.png"
+});
+
+
+document.getElementById("huawei4").addEventListener("mouseover",function(e){
+	
+	e.target.src="huawei-nova-9-2.png"
+});
+document.getElementById("huawei4").addEventListener("mouseout",function(e){
+	
+	e.target.src="huawei-nova-9.png"
+});
+
+
+
+document.getElementById("xiaomi1").addEventListener("mouseover",function(e){
+	
+	e.target.src="Xiaomi-Mi-11-Ultra-2.jpg"
+});
+document.getElementById("xiaomi1").addEventListener("mouseout",function(e){
+
+	e.target.src="xiaomi-mi-11-ultra-5g.jpg"
+});
+
+
+document.getElementById("xiaomi2").addEventListener("mouseover",function(e){
+	
+	e.target.src="xiaomi-mi-11-2.jpg"
+});
+document.getElementById("xiaomi2").addEventListener("mouseout",function(e){
+	
+	e.target.src="xiaomi-mi-11.jpg"
+});
+
+
+document.getElementById("xiaomi3").addEventListener("mouseover",function(e){
+	
+	e.target.src="Xiaomi-Mi10-Pro-5G-2.jpg"
+});
+document.getElementById("xiaomi3").addEventListener("mouseout",function(e){
+	
+	e.target.src="xiaomi-mi10pro.jpg"
+});
+
+
+document.getElementById("xiaomi4").addEventListener("mouseover",function(e){
+	
+	e.target.src="xiaomi-mi-10-ultra-2.jpeg"
+});
+document.getElementById("xiaomi4").addEventListener("mouseout",function(e){
+	
+	e.target.src="xiaomi-mi10-ultra.jpg"
+});
+
+
+
+
+document.getElementById("tecno1").addEventListener("mouseover",function(e){
+	
+	e.target.src="tecno-camon-18-premier-2.jpg"
+});
+document.getElementById("tecno1").addEventListener("mouseout",function(e){
+
+	e.target.src="tecno-camon-18-premier.jpg"
+});
+
+
+document.getElementById("tecno2").addEventListener("mouseover",function(e){
+	
+	e.target.src="tecno-camon-17-pro-2.jpg"
+});
+document.getElementById("tecno2").addEventListener("mouseout",function(e){
+	
+	e.target.src="Tecno-Camon-17-Pro--2.jpg"
+});
+
+
+document.getElementById("tecno3").addEventListener("mouseover",function(e){
+	
+	e.target.src="tecno-pova-2.jpg"
+});
+document.getElementById("tecno3").addEventListener("mouseout",function(e){
+	
+	e.target.src="tecno_pova_2_2.jpg"
+});
+
+
+document.getElementById("tecno4").addEventListener("mouseover",function(e){
+	
+	e.target.src="Tecno-Spark-7-Pro.jpg"
+});
+document.getElementById("tecno4").addEventListener("mouseout",function(e){
+	
+	e.target.src="tecno-spark-7-pro-2.jpg"
+});
+
+
+
+
+
+document.getElementById("oppo1").addEventListener("mouseover",function(e){
+	
+	e.target.src="Oppo-Find-X3-Pro-2.jpg"
+});
+document.getElementById("oppo1").addEventListener("mouseout",function(e){
+
+	e.target.src="Oppo-Find-X3-Pro.png"
+});
+
+
+document.getElementById("oppo2").addEventListener("mouseover",function(e){
+	
+	e.target.src="Oppo-Find-X2-Pro-2.jpg"
+});
+document.getElementById("oppo2").addEventListener("mouseout",function(e){
+	
+	e.target.src="Oppo-Find-X2-pro.jpg"
+});
+
+
+document.getElementById("oppo3").addEventListener("mouseover",function(e){
+	
+	e.target.src="Oppo-Find-N.jpg"
+});
+document.getElementById("oppo3").addEventListener("mouseout",function(e){
+	
+	e.target.src="oppo-find-n-1.jpg"
+});
+
+
+document.getElementById("oppo4").addEventListener("mouseover",function(e){
+	
+	e.target.src="oppo reno 7 pro 2.jpg"
+});
+document.getElementById("oppo4").addEventListener("mouseout",function(e){
+	
+	e.target.src="Oppo-Reno-7-Pro-5G.jpg"
+});
+
+function function3(){
+	document.getElementById("iphone").style.border="2px solid red"
+	document.getElementById("samsung").style.border="2px solid red"
+	document.getElementById("huawei").style.border="2px solid red"
+	document.getElementById("xiaomi").style.border="2px solid red"
+	document.getElementById("tecno").style.border="2px solid red"
+	document.getElementById("oppo").style.border="2px solid red"
+};
+
+function function1(){
+	document.getElementById("iphone").style.border="20px solid red"
+	document.getElementById("samsung").style.border="2px solid red"
+	document.getElementById("huawei").style.border="2px solid red"
+	document.getElementById("xiaomi").style.border="2px solid red"
+	document.getElementById("tecno").style.border="2px solid red"
+	document.getElementById("oppo").style.border="2px solid red"
+};
+function function2(){
+	document.getElementById("iphone").style.border="2px solid red"
+	document.getElementById("samsung").style.border="20px solid red"
+	document.getElementById("huawei").style.border="2px solid red"
+	document.getElementById("xiaomi").style.border="2px solid red"
+	document.getElementById("tecno").style.border="2px solid red"
+	document.getElementById("oppo").style.border="2px solid red"
+};
+function function4(){
+	document.getElementById("iphone").style.border="2px solid red"
+	document.getElementById("samsung").style.border="2px solid red"
+	document.getElementById("huawei").style.border="20px solid red"
+	document.getElementById("xiaomi").style.border="2px solid red"
+	document.getElementById("tecno").style.border="2px solid red"
+	document.getElementById("oppo").style.border="2px solid red"
+};
+function function5(){
+	document.getElementById("iphone").style.border="2px solid red"
+	document.getElementById("samsung").style.border="2px solid red"
+	document.getElementById("huawei").style.border="2px solid red"
+	document.getElementById("xiaomi").style.border="20px solid red"
+	document.getElementById("tecno").style.border="2px solid red"
+	document.getElementById("oppo").style.border="2px solid red"
+};
+function function6(){
+	document.getElementById("iphone").style.border="2px solid red"
+	document.getElementById("samsung").style.border="2px solid red"
+	document.getElementById("huawei").style.border="2px solid red"
+	document.getElementById("xiaomi").style.border="2px solid red"
+	document.getElementById("tecno").style.border="20px solid red"
+	document.getElementById("oppo").style.border="2px solid red"
+};
+function function7(){
+	document.getElementById("iphone").style.border="2px solid red"
+	document.getElementById("samsung").style.border="2px solid red"
+	document.getElementById("huawei").style.border="2px solid red"
+	document.getElementById("xiaomi").style.border="2px solid red"
+	document.getElementById("tecno").style.border="2px solid red"
+	document.getElementById("oppo").style.border="20px solid red"
+};
+
+
+$(document).ready(function() {
+	function anim(){
+    $('.nn').animate({left: '+1090'}, 8000)
+	        .animate({left: '-=1000'}, 8000,anim );
+	};
+	anim();
+  });
+  
+$(document).ready(function() {
+	function anime(){
+    $('.ll').animate({fontSize: '+50'}, 5000)
+           	.animate({fontSize: '-=10'}, 5000 ,anime);
+	};
+	anime();
+  });
+$(document).ready(function() {
+   $(".box").animate({  width: "1500px" ,height:"830px" }, 3000); 
+  });
+  
+$(function() {
+    $('#iphone1').mouseover( function() {
+      $(this).animate({left : '+=10'}, 50).animate({left : '-=10',}, 50).animate({left : '-=10'}, 50).animate({left : '+=10',}, 50);
+    }); 	
+  });
+$(function() {
+    $('#iphone2').mouseover( function() {
+      $(this).animate({left : '+=10'}, 50).animate({left : '-=10',}, 50).animate({left : '-=10'}, 50).animate({left : '+=10',}, 50);
+    }); 	
+  });
+$(function() {
+    $('#iphone3').mouseover( function() {
+      $(this).animate({left : '+=10'}, 50).animate({left : '-=10',}, 50).animate({left : '-=10'}, 50).animate({left : '+=10',}, 50);
+    }); 	
+  });
+$(function() {
+    $('#iphone4').mouseover( function() {
+      $(this).animate({left : '+=10'}, 50).animate({left : '-=10',}, 50).animate({left : '-=10'}, 50).animate({left : '+=10',}, 50);
+    }); 	
+  });
+  
+  
+$(function() {
+    $('#samsung1').mouseover( function() {
+      $(this).animate({left : '+=10'}, 50).animate({left : '-=10',}, 50).animate({left : '-=10'}, 50).animate({left : '+=10',}, 50);
+    }); 	
+  });
+$(function() {
+    $('#samsung2').mouseover( function() {
+      $(this).animate({left : '+=10'}, 50).animate({left : '-=10',}, 50).animate({left : '-=10'}, 50).animate({left : '+=10',}, 50);
+    }); 	
+  });
+$(function() {
+    $('#samsung3').mouseover( function() {
+      $(this).animate({left : '+=10'}, 50).animate({left : '-=10',}, 50).animate({left : '-=10'}, 50).animate({left : '+=10',}, 50);
+    }); 	
+  });
+$(function() {
+    $('#samsung4').mouseover( function() {
+      $(this).animate({left : '+=10'}, 50).animate({left : '-=10',}, 50).animate({left : '-=10'}, 50).animate({left : '+=10',}, 50);
+    }); 	
+  });
+  
+
+$(function() {
+    $('#huawei1').mouseover( function() {
+      $(this).animate({left : '+=10'}, 50).animate({left : '-=10',}, 50).animate({left : '-=10'}, 50).animate({left : '+=10',}, 50);
+    }); 	
+  });
+$(function() {
+    $('#huawei2').mouseover( function() {
+      $(this).animate({left : '+=10'}, 50).animate({left : '-=10',}, 50).animate({left : '-=10'}, 50).animate({left : '+=10',}, 50);
+    }); 	
+  });
+$(function() {
+    $('#huawei3').mouseover( function() {
+      $(this).animate({left : '+=10'}, 50).animate({left : '-=10',}, 50).animate({left : '-=10'}, 50).animate({left : '+=10',}, 50);
+    }); 	
+  });
+$(function() {
+    $('#huawei4').mouseover( function() {
+      $(this).animate({left : '+=10'}, 50).animate({left : '-=10',}, 50).animate({left : '-=10'}, 50).animate({left : '+=10',}, 50);
+    }); 	
+  });
+  
+
+$(function() {
+    $('#xiaomi1').mouseover( function() {
+      $(this).animate({left : '+=10'}, 50).animate({left : '-=10',}, 50).animate({left : '-=10'}, 50).animate({left : '+=10',}, 50);
+    }); 	
+  });
+$(function() {
+    $('#xiaomi2').mouseover( function() {
+      $(this).animate({left : '+=10'}, 50).animate({left : '-=10',}, 50).animate({left : '-=10'}, 50).animate({left : '+=10',}, 50);
+    }); 	
+  });
+$(function() {
+    $('#xiaomi3').mouseover( function() {
+      $(this).animate({left : '+=10'}, 50).animate({left : '-=10',}, 50).animate({left : '-=10'}, 50).animate({left : '+=10',}, 50);
+    }); 	
+  });
+$(function() {
+    $('#xiaomi4').mouseover( function() {
+      $(this).animate({left : '+=10'}, 50).animate({left : '-=10',}, 50).animate({left : '-=10'}, 50).animate({left : '+=10',}, 50);
+    }); 	
+  });
+  
+
+$(function() {
+    $('#tecno1').mouseover( function() {
+      $(this).animate({left : '+=10'}, 50).animate({left : '-=10',}, 50).animate({left : '-=10'}, 50).animate({left : '+=10',}, 50);
+    }); 	
+  });
+$(function() {
+    $('#tecno2').mouseover( function() {
+      $(this).animate({left : '+=10'}, 50).animate({left : '-=10',}, 50).animate({left : '-=10'}, 50).animate({left : '+=10',}, 50);
+    }); 	
+  });
+$(function() {
+    $('#tecno3').mouseover( function() {
+      $(this).animate({left : '+=10'}, 50).animate({left : '-=10',}, 50).animate({left : '-=10'}, 50).animate({left : '+=10',}, 50);
+    }); 	
+  });
+$(function() {
+    $('#tecno4').mouseover( function() {
+      $(this).animate({left : '+=10'}, 50).animate({left : '-=10',}, 50).animate({left : '-=10'}, 50).animate({left : '+=10',}, 50);
+    }); 	
+  });
+  
+
+$(function() {
+    $('#oppo1').mouseover( function() {
+      $(this).animate({left : '+=10'}, 50).animate({left : '-=10',}, 50).animate({left : '-=10'}, 50).animate({left : '+=10',}, 50);
+    }); 	
+  });
+$(function() {
+    $('#oppo2').mouseover( function() {
+      $(this).animate({left : '+=10'}, 50).animate({left : '-=10',}, 50).animate({left : '-=10'}, 50).animate({left : '+=10',}, 50);
+    }); 	
+  });
+$(function() {
+    $('#oppo3').mouseover( function() {
+      $(this).animate({left : '+=10'}, 50).animate({left : '-=10',}, 50).animate({left : '-=10'}, 50).animate({left : '+=10',}, 50);
+    }); 	
+  });
+$(function() {
+    $('#oppo4').mouseover( function() {
+      $(this).animate({left : '+=10'}, 50).animate({left : '-=10',}, 50).animate({left : '-=10'}, 50).animate({left : '+=10',}, 50);
+    }); 	
+  });
